1d330dda9591436069c65fe9ff76dd7f3d9ad3d3
Peace Udotong <udotongpeace@gmail.com>
2026-08-20 21:00:06 +0100

    please work?

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_BASE_URL:
+      process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://baba-sika.onrender.com/api",
+  },
 };
 
 export default nextConfig;
